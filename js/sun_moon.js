5ca7127d98367148c2834d422b13a79d15d0ac4b
qingchenyouforcc <jzwberry666666@163.com>
2024-11-06 10:39:41 +0800

    Site updated: 2024-11-06 10:39:41

--- js/sun_moon.js
+++ js/sun_moon.js
@@ -12,12 +12,12 @@ function switchNightMode() {
     })
   const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
   if (nowMode === 'light') {
-    activateDarkMode()
+    btf.activateDarkMode()
     saveToLocal.set('theme', 'dark', 2)
     GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
     document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')
   } else {
-    activateLightMode()
+    btf.activateLightMode()
     saveToLocal.set('theme', 'light', 2)
     document.querySelector('body').classList.add('DarkMode'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')
   }
